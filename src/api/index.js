54336d5fb6178a04ed4560bd07ddb2cc4be4160a
MuneebZafarQureshi <muneebzafar2@yahoo.com>
2020-07-23 17:49:45 +0500

    Update index.js in src/api
    index.js of sample api is updated...

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
+//const url = 'https://covid-19-data.p.rapidapi.com';
+//const key = 'e46b72bcfamsh4897f60efcb115dp1f9bb4jsn3f6ab321f284';
 export const fetchData = async () => {
 try {
-    const response = await axios.get(url);
+        const response = await axios.get(url);
     
-    return response;
-} catch (error) {
-    
+        return response;
 }
+ catch (error) {   
+}
+
 }

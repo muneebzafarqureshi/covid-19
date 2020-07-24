23545e4969910c6e1ff543ea3fd770e44cc66a54
MuneebZafarQureshi <muneebzafar2@yahoo.com>
2020-07-24 15:18:40 +0500

    Updated JSON packages and some files
    Updated Package-lock.json
    
    Updated package.json
    
    Updated src\api\index.js (removed RAPIDAPI key mentioned as comment)
    
    Updated src\App.js
    
    Updated src\App.module.css
    
    Updated src\components\Cards\Cards.jsx

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,11 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-//const url = 'https://covid-19-data.p.rapidapi.com';
-//const key = 'e46b72bcfamsh4897f60efcb115dp1f9bb4jsn3f6ab321f284';
 export const fetchData = async () => {
 try {
-        const response = await axios.get(url);
-    
-        return response;
+        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        return { confirmed, recovered, deaths, lastUpdate };
 }
  catch (error) {   
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
-
-const Cards = () => {
+import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Cards.module.css';
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }})=> {
+    if(!confirmed){
+        return 'Loading...'
+    }
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+<Grid container spacing={3} justify='center'>
+    <Grid item component={Card}>
+        <CardContent>
+            <Typography color='textSecondary' gutterBottom>Infected</Typography>
+            <Typography varient='h5'>{confirmed.value}</Typography>
+            <Typography color='textSecondary' >REAL DATE</Typography>
+            <Typography varient='body2'>Number of active cases of COVID-19</Typography>
+        </CardContent>
+    </Grid>
+    <Grid item component={Card}>
+        <CardContent>
+            <Typography color='textSecondary' gutterBottom>Recovered</Typography>
+            <Typography varient='h5'>REAL DATA</Typography>
+            <Typography color='textSecondary' >REAL DATE</Typography>
+            <Typography varient='body2'>Number of recoveries from COVID-19</Typography>
+        </CardContent>
+    </Grid>
+    <Grid item component={Card}>
+        <CardContent>
+            <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+            <Typography varient='h5'>REAL DATA</Typography>
+            <Typography color='textSecondary' >REAL DATE</Typography>
+            <Typography varient='body2'>Number of deaths caused by COVID-19</Typography>
+        </CardContent>
+    </Grid>
+</Grid>
+        </div>
     )
 }
 
-export default Cards; 
+export default Cards;  

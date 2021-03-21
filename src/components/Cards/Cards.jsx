e8fd53a5bac7bfe92bd47c631bc2d7a80b5ffd22
muneebzafarqureshi <muneebzafar2@yahoo.com>
2021-03-21 17:12:09 +0500

    updated Cards.jsx file for basic graphics

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid} from '@material-ui/core';
 import styles from './Cards.module.css';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }})=> {
     if(!confirmed){
@@ -10,7 +11,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }})=> {
     return (
         <div className={styles.container}>
 <Grid container spacing={3} justify='center'>
-    <Grid item component={Card}>
+    <Grid item component={Card} xs={12} md={3} className={cx(styles.Card, styles.infected)}>
         <CardContent>
             <Typography color='textSecondary' gutterBottom>Infected</Typography>
             <Typography varient='h5'>
@@ -20,7 +21,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }})=> {
             <Typography varient='body2'>Number of active cases of COVID-19</Typography>
         </CardContent>
     </Grid>
-    <Grid item component={Card}>
+    <Grid item component={Card} xs={12} md={3} className={cx(styles.Card, styles.recovered)}>
         <CardContent>
             <Typography color='textSecondary' gutterBottom>Recovered</Typography>
             <Typography varient='h5'>
@@ -30,7 +31,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }})=> {
             <Typography varient='body2'>Number of recoveries from COVID-19</Typography>
         </CardContent>
     </Grid>
-    <Grid item component={Card}>
+    <Grid item component={Card} xs={12} md={3} className={cx(styles.Card, styles.deaths)}>
         <CardContent>
             <Typography color='textSecondary' gutterBottom>Deaths</Typography>
             <Typography varient='h5'>
